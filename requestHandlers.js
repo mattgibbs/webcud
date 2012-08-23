adaabf9788956e4da32821a9e6a8ef7d2af1fcfb
Matt Gibbs <mgibbs@dhcpvisitor2175.slac.stanford.edu>
2012-08-22 20:12:15 -0700

    Server returns JSON instead of plain text, and returns more info than just the value.

--- requestHandlers.js
+++ requestHandlers.js
@@ -8,11 +8,27 @@ function test(response) {
 
 function PV(response, query) {
 	PVtoGet = query["PV"];
+	var data = {}
 	console.log("Request handler 'PV' was called, with PV = " + PVtoGet + ".");
 	
-	exec("caget" + PVtoGet, {timeout:10000, maxBuffer: 20000*1024}, function(error, stdout, stderr) {
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write(stdout);
+	exec("caget -as " + PVtoGet, {timeout:10000, maxBuffer: 20000*1024}, function(error, stdout, stderr) {
+		if (error == null) {
+			response.writeHead(200, {"Content-Type": "application/json"});
+			
+			//Split the string into an array.  Whitespace denotes a new field.  Get rid of any blank fields.
+			var cagetResults = stdout.split(" ").filter(function(val,index,array){ return (array[index] != "" && array[index] != "\n")});
+			console.log(cagetResults);
+			data = {"PV": cagetResults[0],
+					"value": cagetResults[3],
+			 		"timestamp": cagetResults[1] + " " + cagetResults[2],
+					"status": cagetResults[4],
+					"severity": cagetResults[5]};
+			response.write(JSON.stringify(data));
+		} else {
+			response.writeHead(404,{"Content-Type": "text/plain"});
+			response.write(error.message);
+		}
+		
 		response.end();
 	});
 }

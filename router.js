ab0344120689219728ffdc82454de0336e341c79
Matt Gibbs <mgibbs@dhcpvisitor21972.slac.stanford.edu>
2012-09-07 13:30:21 -0700

    Got rid of some logging, and added Allow-Access-Control-Origin to the 404 response.

--- router.js
+++ router.js
@@ -1,10 +1,10 @@
 function route(handle, pathname, query, response) {
-	console.log("About to route a request for " + pathname);
+	//console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] === 'function') {
 		handle[pathname](response, query);
 	} else {
 		console.log("No request handler found for " + pathname);
-		response.writeHead(404,{"Content-Type": "text/plain"});
+		response.writeHead(404,{"Content-Type": "text/plain","Access-Control-Allow-Origin": "*"});
 		response.write("404 Not found");
 		response.end();
 	}

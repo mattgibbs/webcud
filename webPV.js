1b450fa02c815b4ecd4ff7c147974a0b45d99eaa
Matt Gibbs <mgibbs@RetinaMattBookPro.local>
2012-10-08 12:41:27 -0700

    Emittance numbers now get colors based on their values.

--- webPV.js
+++ webPV.js
@@ -18,6 +18,49 @@ d3.selectAll(".PVmonitor").datum(function() { return this.dataset; }).each(funct
 	bindElementToPV(this, d.pv, d.precision, d.updatetime ? d.updatetime : 3000);
 });
 
+
+//Code for the emittance numbers is specialized so that the colors change.
+var emittanceColorScale = d3.scale.quantile()
+							.domain([0,3])
+							.range(["#00CC22", "#FFFF00", "#FF4000"]);
+							
+d3.selectAll(".emittanceValue").datum(function() { return this.dataset; }).each(function(d) {
+	var elem = this;
+	if (d.updatetime == undefined) {
+		d.updatetime = 3000;
+	}
+	setInterval(function(){
+		d3.json("http://lcls-prod02.slac.stanford.edu:8888/PV?PV=" + d.pv + "&precision=" + d.precision, function(json){
+			d3.select(elem).datum(function(d){ d.value = json["value"]; return d; })
+				.text(function(d,i) { return d.value; })
+				.transition()
+				.duration(500)
+				.style("color",emittanceColorScale(json["value"]));
+				
+		});
+	},d.updatetime);
+});
+
+var matchingColorScale = d3.scale.quantile()
+							.domain([1,1.5])
+							.range(["#00CC22", "#FFFF00", "#FF4000"]);
+							
+d3.selectAll(".matchingValue").datum(function() { return this.dataset; }).each(function(d) {
+	var elem = this;
+	if (d.updatetime == undefined) {
+		d.updatetime = 3000;
+	}
+	setInterval(function(){
+		d3.json("http://lcls-prod02.slac.stanford.edu:8888/PV?PV=" + d.pv + "&precision=" + d.precision, function(json){
+			d3.select(elem).datum(function(d){ d.value = json["value"]; return d; })
+				.text(function(d,i) { return d.value; })
+				.transition()
+				.duration(500)
+				.style("color",matchingColorScale(json["value"]));
+		});
+	},d.updatetime);
+});
+
 function bindElementToPV(elem, PV, precision, updateRate) {
 	//Enforce a maximum update rate of 1 Hz.
 	if(updateRate < 1000){

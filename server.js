7f4d9177f6e80102002adcb7a545e442b82befc4
Matt Gibbs <mgibbs@dhcpvisitor219167.slac.stanford.edu>
2013-04-09 09:13:02 -0700

    Comment out socket.io handling, it somehow causes to requests to get sent to the HTTP server, even when using the non-socket version of the page.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var http = require("http");
 var url = require("url");
-var socketConnection = require("./requestHandlers").socketConnection;
+//var socketConnection = require("./requestHandlers").socketConnection;
 
 function start(route, handle) {
 	function onRequest(request, response) {
+		console.log(request.url);
 		var parsedURL = url.parse(request.url,true);
 		var pathname = parsedURL.pathname;
 		var query = parsedURL.query;
@@ -12,8 +13,8 @@ function start(route, handle) {
 	var pvserver = http.createServer(onRequest).listen(8888);
 	
 	//Set up Socket.IO for websockets support.
-	var io = require('socket.io').listen(pvserver);
-	io.sockets.on('connection', socketConnection);
+	//var io = require('socket.io').listen(pvserver);
+	//io.sockets.on('connection', socketConnection);
 }
 
 console.log("Server is running...");

e093f067671f8d0f51d0640c682880b82a160647
Matt Gibbs <mgibbs@dhcpvisitor222194.slac.stanford.edu>
2013-08-21 14:27:04 -0700

    Separate the general-purpose webPV-HTTP.js file from webcud-specific code.

--- webPV-HTTP.js
+++ webPV-HTTP.js
@@ -1,4 +1,6 @@
-//Get units for each PVmonitor, then bind the elements to the PV server so that they update in real-time.
+/*  webPV-HTTP.js
+    This code looks for elements with the '.PVmonitor' class in your HTML file, and turns them into constantly updating PV monitors.    
+*/
 var PV_URL = "http://lcls-prod03.slac.stanford.edu:8888/PV?PV=";
 
 d3.selectAll(".PVmonitor").datum(function() { 
@@ -12,117 +14,6 @@ d3.selectAll(".PVmonitor").datum(function() {
 		d3.select(this).text("?");
 		bindElementToPV(this, d.pv, d.precision, d.updatetime ? d.updatetime : 2000);
 	}
-});
-
-
-//Code for the emittance numbers is specialized so that the colors change.
-var emittanceColorScale = d3.scale.quantile()
-							.domain([0, 3])
-							.range(["#00CC22", "#FFFF00", "#FF4000"]);
-
-var ageOpacityScale = d3.scale.linear()
-						.domain([0, 16*60*60*1000]) //Make the values fade to 30% after 16 hours.
-						.range([1, 0.3])
-						.clamp(true);
-							
-d3.selectAll(".emittanceValue").datum(function() { return getDataAttributes(this); }).each(function(d) {
-	var elem = this;
-	if (d.updatetime == undefined) {
-		d.updatetime = 3000;
-	}
-	setInterval(function(){
-		d3.json(PV_URL + d.pv, function(error, json){
-			if (error) return console.log("There was an error loading " + PV_URL + d.pv);
-			d3.select(elem).datum(function(d){
-					d.timestamp = json["timestamp"];
-					if (typeof json.value === 'number') {
-						d.value = json.value.toFixed(d.precision);
-					} else {
-						d.value = json.value;
-					}
-					return d;
-			 })
-				.text(function(d,i) { return d.value; })
-				.style("opacity",ageOpacityScale(Number(new Date()) - d.timestamp))
-				.style("color",emittanceColorScale(json["value"]));
-				
-		});
-	},d.updatetime);
-});
-
-var matchingColorScale = d3.scale.quantile()
-							.domain([1, 1.5])
-							.range(["#00CC22", "#FFFF00", "#FF4000"]);
-							
-d3.selectAll(".matchingValue").datum(function() { return getDataAttributes(this); }).each(function(d) {
-	var elem = this;
-	if (d.updatetime == undefined) {
-		d.updatetime = 3000;
-	}
-	setInterval(function(){
-		d3.json(PV_URL + d.pv, function(error, json){
-			if (error) return console.log("There was an error loading " + PV_URL + d.pv);
-			d3.select(elem).datum(function(d){
-					d.timestamp = json["timestamp"];
-					if (typeof json.value === 'number') {
-						d.value = json.value.toFixed(d.precision);
-					} else {
-						d.value = json.value;
-					}
-					return d;
-				})
-				.text(function(d,i) { return d.value; })
-				.style("opacity",ageOpacityScale(Number(new Date()) - d.timestamp))
-				.style("color",matchingColorScale(json["value"]));
-		});
-	},d.updatetime);
-});
-
-bindElementToPV("#amplificationMode","SIOC:SYS0:ML00:CALC998",0,3000,function(val){
-	if (val == "0") {
-		return "Seeded";
-	} else {
-		return "SASE";
-	}
-});
-
-
-setInterval(function(){
-	d3.json(PV_URL + "IOC:BSY0:MP01:REQBYKIKBRST", function(error, json){
-		if (error) return console.log("There was an error loading " + PV_URL + "IOC:BSY0:MP01:REQBYKIKBRST");
-		if (json.value == "Yes") { 
-			d3.select("h2#burstMessage").transition().style("visibility","visible");
-		} else {
-			d3.select("h2#burstMessage").transition().style("visibility","hidden");
-		}
-	}, 3000);
-	
-	d3.json(PV_URL + "IOC:IN20:EV01:BYKIK_ABTACT", function(error, json){
-		if (error) return console.log("There was an error loading " + PV_URL + "IOC:IN20:EV01:BYKIK_ABTACT");
-		if (json.value == "Enable") {
-			d3.json(PV_URL + "IOC:IN20:EV01:BYKIK_ABTPRD", function(json){
-				if (typeof json.value === 'number' && json.value < 2800) {
-					d3.select("h2#abortMessage").transition().style("visibility","visible");
-				} else {
-					d3.select("h2#abortMessage").transition().style("visibility","hidden");
-				}
-			});
-		} else {
-			d3.select("h2#abortMessage").transition().style("visibility","hidden");
-		}
-	}, 3000);
-});
-
-var vernierElement = d3.select("#L3Vernier").datum(function() { return getDataAttributes(this); }).each(function(d) {
-	bindElementToPV(this,d.pv,d.precision,2000,function(val){
-		if (val == 0) {
-			return "";
-		} else {
-			var sign = val > 0 ? "+" : "-";
-			val = val.toFixed(d.precision);
-			return " " + sign + " " + Math.abs(val) + " MeV";
-		}
-	});
 });
 
 function bindElementToPV(elem, PV, precision, updateRate, processor) {
